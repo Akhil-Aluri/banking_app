ac0fc2d9255ae5f9258dd9ff4051225bb97d1f68
Aluri <Akhil.Aluri@harman.com>
2026-03-23 09:35:15 +0530

    Implement event driven async communication using KAFKA using SCF and Streams

---
+++ section_14/loans/src/main/java/com/aluri/loans/controller/LoansController.java
@@ -0,0 +1,104 @@
+package com.aluri.loans.controller;
+
+import com.aluri.loans.constant.LoanConstants;
+import com.aluri.loans.dto.LoansContactInfoDto;
+import com.aluri.loans.dto.LoansDto;
+import com.aluri.loans.dto.ResponseDto;
+import com.aluri.loans.service.LoanService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/loans")
+public class LoansController {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoansController.class);
+    private LoanService loansService;
+    public LoansController(LoanService loansService){
+        this.loansService = loansService;
+    }
+
+    @Autowired
+    private Environment environment;
+
+    @Value("${build.version}")
+    private String buildVersion;
+
+    @Autowired
+    private LoansContactInfoDto loansContactInfoDto;
+
+    @GetMapping("/getLoans")
+    public ResponseEntity<LoansDto> getLoanDetails(@RequestParam
+                                                       @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+                                                       String mobileNumber,
+                                                   @RequestHeader("aluri-correlation-id") String correlationId) {
+        logger.debug("Aluri-Correlation-ID found: {}" , correlationId);
+        LoansDto loansDto = loansService.fetchLoan(mobileNumber);
+        return ResponseEntity.status(HttpStatus.OK).body(loansDto);
+
+    }
+
+    @GetMapping("/build-info")
+    public ResponseEntity<String> getBuildVersion(){
+        return ResponseEntity.status(HttpStatus.OK).body(buildVersion);
+    }
+
+    @GetMapping("/contact-info")
+    public ResponseEntity<LoansContactInfoDto> getContactInfo(){
+        logger.debug("Invoked loans contact-info API");
+        return ResponseEntity.status(HttpStatus.OK).body(loansContactInfoDto);
+    }
+
+    @PostMapping("/createLoan")
+    public ResponseEntity<ResponseDto> createLoan(@RequestParam
+                                                      @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+                                                      String mobileNumber) {
+        loansService.createLoan(mobileNumber);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(LoanConstants.STATUS_201, LoanConstants.MESSAGE_201));
+
+    }
+
+    @PutMapping("/updateLoan")
+    public ResponseEntity<ResponseDto> updateLoanDetails(@Valid @RequestBody LoansDto loansDto) {
+        boolean isUpdated = loansService.updateLoan(loansDto);
+        if(isUpdated) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(LoanConstants.STATUS_200, LoanConstants.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(LoanConstants.STATUS_417, LoanConstants.MESSAGE_417_UPDATE));
+        }
+
+    }
+
+    @DeleteMapping("/closeLoan")
+    public ResponseEntity<ResponseDto> closeLoan(@RequestParam
+                                                     @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+                                                     String mobileNumber) {
+        boolean isDeleted = loansService.closeLoan(mobileNumber);
+        if(isDeleted) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(LoanConstants.STATUS_200, LoanConstants.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(LoanConstants.STATUS_417, LoanConstants.MESSAGE_417_DELETE));
+        }
+
+    }
+}
